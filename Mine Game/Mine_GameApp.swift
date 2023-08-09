95019821736db005cf12cf9e57f0fa4c035278fa
LongLong0606 <goldsky284@gmail.com>
2023-08-09 13:16:22 +0700

    Initial Commit

---
+++ Mine Game/Mine_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mine_GameApp.swift
+//  Mine Game
+//
+//  Created by Phạm Long on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Mine_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
